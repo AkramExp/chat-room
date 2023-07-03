930909a1125a0ef9958456dc742c748361e95dc5
Akram Ali <akram.exp02@gmail.com>
2023-07-03 14:41:32 +0530

    Add update room functionality

--- base/models.py
+++ base/models.py
@@ -10,14 +10,16 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-    # host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=80)
     description = models.TextField(null=True, blank=True)
     # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
     def __str__(self):
         return self.name
 
