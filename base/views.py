28887c6fcb2920c0b25270e821cc2587d5b96720
Akram Ali <akram.exp02@gmail.com>
2023-07-03 20:25:16 +0530

    Add texting functionality

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -74,7 +74,16 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        messages = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
+    context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html/', context)
 
 
