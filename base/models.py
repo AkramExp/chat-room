b7a9690c2e8fd5a6edc473a5189a63b641b968d7
Akram Ali <akram.exp02@gmail.com>
2023-07-03 12:09:03 +0530

    Create message and user model

--- base/models.py
+++ base/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Room(models.Model):
@@ -12,3 +20,14 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:50]
