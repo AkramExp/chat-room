8dca26f37957ed05f24d1603db8b76a74ed97050
Akram Ali <akram.exp02@gmail.com>
2023-07-03 23:09:38 +0530

    Add activity feed

--- base/views.py
+++ base/views.py
@@ -68,7 +68,8 @@ def home(request):
     topics = Topic.objects.all()
 
     room_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    room_messages = Message.objects.all()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
