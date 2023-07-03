a48c547ad1ab2e056bad249e484f59dd71e89670
Akram Ali <akram.exp02@gmail.com>
2023-07-03 18:24:42 +0530

    Add login functionality

--- base/views.py
+++ base/views.py
@@ -2,10 +2,33 @@ from django.db.models import Q
 from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+
 
 def loginPage(request):
-    context= {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User Doesnt Exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'username or password does not exist')
+
+    context = {}
     return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
