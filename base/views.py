3cd5510f1ae29e6caafa3f7eff0d90b33fd4a92c
Akram Ali <akram.exp02@gmail.com>
2023-07-05 16:59:41 +0530

    Change model of update user form

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
@@ -163,4 +163,12 @@ def userProfile(request, pk):
 
 @login_required(login_url='login')
 def updateUser(request):
-    return render(request, 'base/update_user.html')
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+    return render(request, 'base/update_user.html', {'form': form})
