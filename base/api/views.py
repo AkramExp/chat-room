057f29028db311bb1c11c1148bfb7c8a14b4947a
Akram Ali <akram.exp02@gmail.com>
2023-07-05 22:10:31 +0530

    Made an api for the website

--- base/api/views.py
+++ base/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from base.models import Room
+from .serializers import RoomSerializer
 
 
 @api_view(['GET'])
@@ -10,3 +12,16 @@ def getRoutes(request):
         'GET /api/room/:id'
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getRooms(request):
+    rooms = Room.objects.all()
+    serializer = RoomSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    serializer = RoomSerializer(room, many=False)
+    return Response(serializer.data)
