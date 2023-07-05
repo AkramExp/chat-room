4a3940b40e6ad5952afa0db9685d829b827689a1
Akram Ali <akram.exp02@gmail.com>
2023-07-05 17:04:05 +0530

    fix messages not loading

--- base/views.py
+++ base/views.py
@@ -75,7 +75,7 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('created')
+    room_messages = room.message_set.all().order_by('-created')
     participants = room.participants.all()
     print(room.participants)
     if request.method == 'POST':
